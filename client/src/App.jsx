8faaca1f36b3bba22618cc1765244dfc22b46d0d
Abhishek-3191 <abhisheksrivastava49631@gmail.com>
2025-06-07 23:47:36 +0530

    App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,31 @@
 import Button from ".components/ui/button";
-
+import { Route,Routes } from "react-router-dom";
+import {AuthPage} from "./pages/auth"
+import RouteGuard from "./components/route-guard";
+import { useContext } from "react";
+import { AuthContext } from "./context/auth-context";
 
 function App(){
+  const {auth}=useContext(AuthContext);
   return (
     <Routes>
-      <Route path="/auth" element={<AuthPage/>}/>
+      <Route path="/auth" element={<RouteGuard 
+      element={<AuthPage/>}
+      authenticated={auth?.authenticated}
+      user={auth?.user}
+
+      />}
+      />
+      <Route
+        path="/instructor"
+        element={
+          <RouteGuard
+            element={<InstructorDashboardpage />}
+            authenticated={auth?.authenticate}
+            user={auth?.user}
+          />
+        }
+      />
     </Routes>
   );
 }
