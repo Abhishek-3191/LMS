dea30468aac28f4d75980769611535f33c56bf87
Abhishek-3191 <abhisheksrivastava49631@gmail.com>
2025-06-07 23:44:58 +0530

    routes

--- server/routes/auth-routes/index.js
+++ server/routes/auth-routes/index.js
@@ -1,7 +1,18 @@
 const express=require('express');
-const {registeredUser}=require('../../controllers/auth-controller/index');
+const {registerUser,loginUser,logoutUser}=require('../../controllers/auth-controller/index');
 const router=express.Router();
+const {authenticateMiddleware}=require('../../middleware/auth-middleware');
 
-router.post('/register',registeredUser);
+router.post('/register',registerUser);
+router.post('/login',loginUser);
+// router.post('/logout',logoutUser);
+router.get('/check-auth',authenticateMiddleware,(req,res)=>{
+    const user=req.user;
+    res.status(200).json({
+        success:true,
+        message:"User authenticated!",
+        data:user 
+    })
+});
 
 module.exports=router;
