1615222708162561167729689af43c91c347b0ae
Abhishek-3191 <abhisheksrivastava49631@gmail.com>
2025-06-09 18:06:16 +0530

    backend

--- server/server.js
+++ server/server.js
@@ -3,21 +3,26 @@ const express=require("express");
 const cors=require('cors');
 const mongoose=require('mongoose');
 const app=express();
+const authRoutes=require('./routes/auth-routes/index');
 
 const PORT=process.env.PORT || PORT;
 
 const MONGO_URI=process.env.MONGO_URI;
-
+app.use(
 cors({
     origin:process.env.CLIENT_URL,
     methods:['GET','POST','PUT','DELETE'],
     allowedHeaders:['Content-Type','Authorization']
-});
+})
+);
 
 app.use(express.json());
 
 mongoose.connect(MONGO_URI).then(()=>console.log("MONGODB is connected"))
 .catch((e)=>console.log(e));
+
+//Routing
+app.use('/auth',authRoutes);
 
 app.use((err,req,res,next)=>{
     res.status(500).json({success:false,message:'Something went wrong'});
